1987c8ef4c6c087b250b05f5f56b336743336396
Agnija <ilzena.agnija@gmail.com>
2021-10-08 14:45:53 +0200

    JS if statements added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-
-const randomNumber1 =Math.floor(Math.random()*6)+1;
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 const randomImageSource = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
-
-
-const randomNumber2 = Math.floor(Math.random()*6)+1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 const randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// if (randomNumber1 > randomNumber2)
+// {document.querySelector("h1").textContent = "🎈Player 1 Wins!"} ;
+
+// if(randomNumber2 > randomNumber1)
+// {document.querySelector("h1").textContent = "Player 2 Wins! 🎈"}
+
+// if(randomNumber1 == randomNumber2)
+// {document.querySelector("h1").textContent = "Draw!"}
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "🎈Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").textContent = "Player 2 Wins! 🎈";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}
